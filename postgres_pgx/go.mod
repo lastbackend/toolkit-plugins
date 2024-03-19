4c333dec1de205384f8ea96c1576b05fbde13b32
K. Pastor-Gerte <konstantin@lastbackend.com>
2024-03-19 17:04:01 +0300

    add postgres_pgx plugin

---
+++ postgres_pgx/go.mod
@@ -0,0 +1,43 @@
+module github.com/lastbackend/toolkit-plugins/postgres_pgx
+
+go 1.21.5
+
+require (
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/golang-migrate/migrate/v4 v4.17.0
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/lastbackend/toolkit v0.0.0-20240115154229-332ae80d9328
+	github.com/lib/pq v1.10.9
+	github.com/pkg/errors v0.9.1
+	github.com/caarlos0/env/v7 v7.1.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/fx v1.20.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20240108191215-35c7eff3a6b1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
---
+++ postgres_pgx/plugin.go
@@ -0,0 +1,224 @@
+/*
+Copyright [2014] - [2023] The Last.Backend authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package postgres_pgx
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/golang-migrate/migrate/v4"
+	mpgx "github.com/golang-migrate/migrate/v4/database/pgx"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/lastbackend/toolkit/pkg/runtime"
+	"github.com/lastbackend/toolkit/pkg/runtime/logger"
+	"github.com/lastbackend/toolkit/pkg/tools/probes"
+	"github.com/pkg/errors"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file" // nolint
+	_ "github.com/lib/pq"
+)
+
+const (
+	defaultPrefix = "psql"
+	driverName    = "postgres"
+)
+
+const (
+	errMissingConnectionString = "Missing connection string"
+	errConnectAttempts         = "Connect attempts failed"
+)
+
+type Plugin interface {
+	DB() *pgxpool.Pool
+	RunMigration() error
+}
+
+type Options struct {
+	Name string
+}
+
+type Config struct {
+	DSN           string        `env:"DSN"  envDefault:"" comment:"DSN = postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...] complete connection string"`
+	Host          string        `env:"HOST" envDefault:"127.0.0.1"  comment:"The host to connect to (required)"`
+	Port          int32         `env:"PORT" envDefault:"5432" comment:"The port to bind to (default: 5432)"`
+	Database      string        `env:"DATABASE" comment:"Database to be selected after connecting to the server."`
+	Username      string        `env:"USERNAME" comment:"The username to connect with. Not required if using IntegratedSecurity"`
+	Password      string        `env:"PASSWORD" comment:"The password to connect with. Not required if using IntegratedSecurity"`
+	SSLMode       string        `env:"SSLMODE" comment:" Whether or not to use SSL mode (disable, allow, prefer, require, verify-ca, verify-full)"`
+	TimeZone      string        `env:"TIMEZONE" comment:"Sets the session timezone"`
+	MaxPoolSize   int           `env:"MAX_POOL_SIZE" envDefault:"2" comment:"Sets the max pool size"`
+	ConnAttempts  int           `env:"CONN_ATTEMPTS" envDefault:"10" comment:"Sets the connection attempts"`
+	ConnTimeout   time.Duration `env:"CONN_TIMEOUT" envDefault:"15s" comment:"Sets the connection timeout"`
+	MigrationsDir string        `env:"MIGRATIONS_DIR" comment:"Migrations directory to run migration when plugin is started"`
+}
+
+type plugin struct {
+	log     logger.Logger
+	runtime runtime.Runtime
+
+	prefix     string
+	envPrefix  string
+	connection string
+	opts       Config
+
+	builder squirrel.StatementBuilderType
+	pool    *pgxpool.Pool
+}
+
+func NewPlugin(runtime runtime.Runtime, opts *Options) Plugin {
+	p := new(plugin)
+	p.runtime = runtime
+	p.log = runtime.Log()
+
+	p.prefix = opts.Name
+	if p.prefix == "" {
+		p.prefix = defaultPrefix
+	}
+
+	if err := runtime.Config().Parse(&p.opts, p.prefix); err != nil {
+		return nil
+	}
+
+	return p
+}
+
+func (p *plugin) DB() *pgxpool.Pool {
+	return p.pool
+}
+
+func (p *plugin) PreStart(ctx context.Context) (err error) {
+
+	if p.opts.DSN == "" {
+		if p.opts.Host == "" {
+			return fmt.Errorf("%s_DSN or %s_Host environment variable required but not set",
+				p.prefix, p.prefix)
+		}
+		p.opts.DSN = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+			p.opts.Username, p.opts.Password, p.opts.Host, p.opts.Port, p.opts.Database, p.opts.SSLMode)
+	}
+
+	p.builder = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+	poolConfig, err := pgxpool.ParseConfig(p.opts.DSN)
+	if err != nil {
+		return errors.New(errMissingConnectionString)
+	}
+
+	poolConfig.MaxConns = int32(p.opts.MaxPoolSize)
+
+	connAttempts := p.opts.ConnAttempts
+
+	for connAttempts > 0 {
+		p.pool, err = pgxpool.NewWithConfig(ctx, poolConfig)
+		if err == nil {
+			break
+		}
+
+		log.Printf("Postgres is trying to connect, attempts left: %d", connAttempts)
+
+		time.Sleep(p.opts.ConnTimeout)
+
+		connAttempts--
+	}
+
+	if err != nil {
+		return errors.New(errConnectAttempts)
+	}
+
+	db, err := pgx.Connect(ctx, p.opts.DSN)
+	if err != nil {
+		return errors.New(errMissingConnectionString)
+	}
+	p.connection = p.opts.DSN
+
+	p.runtime.Tools().Probes().RegisterCheck(p.prefix, probes.ReadinessProbe, PostgresPingChecker(db, 1*time.Second))
+
+	return nil
+}
+
+func (p *plugin) OnStop(context.Context) error {
+	if p.pool != nil {
+		p.pool.Close()
+	}
+	return nil
+}
+
+func PostgresPingChecker(database *pgx.Conn, timeout time.Duration) probes.HandleFunc {
+	return func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		if database == nil {
+			return fmt.Errorf("connection is nil")
+		}
+		return database.Ping(ctx)
+	}
+}
+
+func (p *plugin) RunMigration() error {
+
+	if p.opts.MigrationsDir == "" {
+		return fmt.Errorf("can not run migration: dir is not set: %s", p.opts.MigrationsDir)
+	}
+
+	fmt.Printf("\nRun migration from dir: %s", p.opts.MigrationsDir)
+
+	opts, err := pgx.ParseConfig(p.opts.DSN)
+	if err != nil {
+		return errors.New(errMissingConnectionString)
+	}
+
+	conn, err := sql.Open(driverName, p.opts.DSN)
+	if err != nil {
+		return fmt.Errorf("failed to dbection open: %w", err)
+	}
+
+	driver, err := mpgx.WithInstance(conn, &mpgx.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(fmt.Sprintf("file://%s", p.opts.MigrationsDir), opts.Database, driver)
+	if err != nil {
+		return err
+	}
+
+	version, dirty, err := m.Version()
+	if err != nil && err != migrate.ErrNilVersion {
+		return err
+	}
+	if dirty {
+		if err := m.Force(int(version)); err != nil {
+			return err
+		}
+		if err := m.Down(); err != nil {
+			return err
+		}
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	fmt.Printf("\nMigration completed!\n")
+
+	return nil
+}
